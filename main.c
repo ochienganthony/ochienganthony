5f274b4dd742b4aec73c9ee0d20bddc51ef724de
Anthony <ochienganthony001@gmail.com>
2023-10-05 10:09:10 +0300

    awesome project

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float a, b;
+    printf (" Enter the first value:");
+    scanf("%f", &a );
+    printf (" Enter the second value:");
+    scanf("%f", &b );
+    while (a<=100)
+    {
+        if ((a>98.6) && (b<98.6))
+        {
+            printf("%6.2f degrees F=%6.2f degrees C\n",98.6, (98.6-32.0) * 5.0/9.0);
+        }
+        printf("%6.2f degrees F=%6.2f degrees C\n",a, (a-32.0) * 5.0/9.0);
+        b = a;
+        a = a + 10;
+    }
+
+    return 0;
+}
+
